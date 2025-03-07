dc472c48cd8bcd3c9730f3eccb4e9ef43f4e7846
Ubaydullayeww <ubaydullayeww72@gmail.com>
2025-03-07 15:21:30 +0500

    fgfggg

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(MyApp());
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
+
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Center(
+        child:  Text('data'),
+      ),
+    );
+  }
+}
